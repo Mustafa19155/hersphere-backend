549a7416f9a117f7835e3061ac8007c3d9892aa7
Mustafa19155 <mustafahassan058@gmail.com>
2024-03-26 00:49:29 +0500

    removed mobile app

--- client/utils/CapitalizeFirst.js
+++
@@ -1,3 +0,0 @@
-export default capitalize = str => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
--- client/variables.js
+++
@@ -1,2 +0,0 @@
-export const baseURL = 'http://192.168.18.55:3001';
-// export const baseURL = 'http://172.20.10.3:3001';
